898ebd2e79a837d2fbaaf611962caddff02a5d1d
Naomi Blindeman <naomi@blindeman.com>
2019-09-04 21:41:32 -0700

    initial commit

--- header.php
+++ header.php
@@ -8,18 +8,20 @@
     <body <?php body_class(); ?>>
         <div id="wrapper" class="hfeed">
             <header id="header">
-                <nav id="menu">
-                    <div class="primary-menu-container width">
-                        <button class="menu-toggle"><span class="screen-reader-text">Primary Menu</span></button>
-                        <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => '' ) ); ?>
+                <div class="width">
+                    <div id="branding">
+                        <div id="site-title">
+                            <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
+                                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
+                            <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
+                        </div>
                     </div>
-                </nav>
-                <div id="branding" class="width">
-                    <div id="site-title">
-                        <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
-                            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
-                        <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
-                    </div>
+                    <nav id="menu">
+                        <div class="primary-menu-container">
+                            <button class="menu-toggle"><span class="screen-reader-text">Primary Menu</span></button>
+                            <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => '' ) ); ?>
+                        </div>
+                    </nav>
                 </div>
             </header>
             <div id="container">
